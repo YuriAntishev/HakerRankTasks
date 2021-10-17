d6e6d521687dbb3788a11bf1d718c1ef3d1823a8
Iurii Antyshev <iurii@beckndevs.com>
2021-10-17 11:16:31 +0300

    make it better

--- DayOfTheProgrammer/solution.js
+++ DayOfTheProgrammer/solution.js
@@ -30,17 +30,11 @@ function readLine() {
  */
 
 function dayOfProgrammer(year) {
-  // if (year == 1918)
-  //     return "26.09.1918";
-  // else if (((year <= 1917) && (year%4 == 0)) || ((year%400 == 0) || ((year%4 ==0) & (year%100 != 0))))
-  //     return "12.09." + year;
-  // else
-  //     return "13.09." + year;
-  //   if (year === 1800 || year === 1900 || year === 1700) return "12.09." + year;
-  //   if (year === 1918) return "26.09." + year;
-  //   let leap = new Date(year, 1, 29).getDate() === 29;
-  //   console.log(year + " : " + leap);
-  //   return leap === true ? "12.09." + year : "13.09." + year;
+  if (year === 1800 || year === 1900 || year === 1700) return "12.09." + year;
+  if (year === 1918) return "26.09." + year;
+  let leap = new Date(year, 1, 29).getDate() === 29;
+  console.log(year + " : " + leap);
+  return leap === true ? "12.09." + year : "13.09." + year;
 }
 
 function main() {
